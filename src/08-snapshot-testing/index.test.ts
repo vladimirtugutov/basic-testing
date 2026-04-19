62134b1c74968f821102c0b2c69f77723c6fac7f
Vladimir Tugutov <tugutov@gmail.com>
2026-04-19 17:15:44 -0300

    feat: add more generateLinkedList tests

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -22,4 +22,40 @@ describe('generateLinkedList', () => {
     const list = generateLinkedList(['a', 'b', 'c']);
     expect(list).toMatchSnapshot();
   });
+
+  test('should return empty linked list for empty array', () => {
+    const list = generateLinkedList([]);
+
+    expect(list).toStrictEqual({
+      value: null,
+      next: null,
+    });
+  });
+
+  test('should generate linked list for single element array', () => {
+    const list = generateLinkedList([42]);
+
+    expect(list).toStrictEqual({
+      value: 42,
+      next: {
+        value: null,
+        next: null,
+      },
+    });
+  });
+
+  test('should preserve null values inside array as node values', () => {
+    const list = generateLinkedList([null, 1]);
+
+    expect(list).toStrictEqual({
+      value: null,
+      next: {
+        value: 1,
+        next: {
+          value: null,
+          next: null,
+        },
+      },
+    });
+  });
 });
